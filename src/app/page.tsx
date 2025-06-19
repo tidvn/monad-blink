58dc4891bedac16842402a4d29b3e04a2fa23da1
tidvn <tiendung0325@gmail.com>
2025-06-19 09:59:25 +0000

    Update API URL in Home component to use production endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
 
   // Action we want to execute in the Blink
   const { blink, isLoading } = useBlink({
-    url: "evm-action:http://localhost:3000/api/actions/donate-mon",
+    url: "evm-action:https://monad-blink-chi.vercel.app/api/actions/donate-mon",
   });
 
   return (
